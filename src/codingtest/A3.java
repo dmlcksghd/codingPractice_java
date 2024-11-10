893e86ef53b2b2080f97925c6b737cb282eb974e
HongEC <honlucete@naver.com>
2024-11-10 13:47:19 +0900

    ...

---
+++ src/codingtest/A3.java
@@ -0,0 +1,29 @@
+package codingtest;
+
+import java.util.Scanner;
+
+public class A3 {
+
+	public static void main(String[] args) {
+		// 한 개의 문장이 주어지면 그 문장 속에서 가장 긴 단어를 출력
+		Scanner sc = new Scanner(System.in);
+
+		String input = sc.nextLine();
+
+		System.out.println(solution(input));
+		sc.close();
+	}
+
+	private static String solution(String input) {
+		String[] inputArr = input.split(" ");
+		int result = 0;
+		
+		for(int i=1; i<inputArr.length; i++) {
+			if(inputArr[i].length() > inputArr[result].length()) {
+				result = i;
+			}
+		}
+		return inputArr[result];
+	}
+
+}

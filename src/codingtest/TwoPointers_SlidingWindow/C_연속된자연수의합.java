f440f294c75bac3620f2bcb4665f5f57ff900ab3
HongEC <honlucete@naver.com>
2024-12-02 22:09:04 +0900

    ...

--- src/codingtest/TwoPointers_SlidingWindow/C_연속된자연수의합.java
+++ src/codingtest/TwoPointers_SlidingWindow/C_연속된자연수의합.java
@@ -10,23 +10,24 @@ public class C_연속된자연수의합 {
 		int n = sc.nextInt();
 		
 		System.out.println(solution(n));
+		
+		sc.close();
 	}
 
 	private static int solution(int n) {
-		int result = 0, sum = 0, lt = 0;
-		int m = n / 2 + 1;
+		int result=0, sum=0, lt=0;
+		int m=n/2+1;
 		int[] arr = new int[m];
 		
-		for(int i=0; i<m; i++) arr[i] = i + 1;
+		for(int i=0; i<m; i++) arr[i] = i+1;
 		for(int rt=0; rt<m; rt++) {
 			sum += arr[rt];
-			if(sum==n) result++;
+			if(sum == n) result++;
 			while(sum>=n) {
 				sum -= arr[lt++];
-				if(sum==n) result++;
+				if(sum == n) result++;
 			}
 		}
 		return result;
 	}
-
 }

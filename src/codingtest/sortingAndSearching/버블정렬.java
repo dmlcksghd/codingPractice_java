a153c68169c2830851a653c261cff94f02ad271a
HongEC <honlucete@naver.com>
2024-12-11 08:56:37 +0900

    ...

--- src/codingtest/sortingAndSearching/버블정렬.java
+++ src/codingtest/sortingAndSearching/버블정렬.java
@@ -2,13 +2,14 @@ package codingtest.sortingAndSearching;
 
 import java.util.*;
 
-public class F_선택정렬 {
+public class 버블정렬 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		
 		int n = sc.nextInt();
-		int[] arr= new int[n];
+		int[] arr = new int[n];
 		for(int i=0; i<n; i++) {
 			arr[i] = sc.nextInt();
 		}
@@ -16,19 +17,18 @@ public class F_선택정렬 {
 		for(int x : solution(n, arr)) {
 			System.out.print(x + " ");
 		}
-		
 		sc.close();
 	}
 
 	private static int[] solution(int n, int[] arr) {
 		for(int i=0; i<n-1; i++) {
-			int idx=i;
-			for(int j=i+1; j<n; j++) {
-				if(arr[j]<arr[idx]) idx=j;
+			for(int j=0; j<n-i-1; j++) {
+				if(arr[j]>arr[j+1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+				}
 			}
-			int tmp=arr[i];
-			arr[i]=arr[idx];
-			arr[idx]=tmp;
 		}
 		return arr;
 	}

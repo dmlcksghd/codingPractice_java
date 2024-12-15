029ce38670c10ec03d87c64223a7686bc14ecd73
HongEC <honlucete@naver.com>
2024-12-16 08:45:09 +0900

    ...

--- src/codingtest/sortingAndSearching/F_LeastRecentlyUsed.java
+++ src/codingtest/sortingAndSearching/F_LeastRecentlyUsed.java
@@ -33,13 +33,12 @@ public class F_LeastRecentlyUsed {
 				for(int i=size-1; i>=1; i--) {
 					cache[i] = cache[i-1];
 				}
-				cache[0] = x;
 			} else {
 				for(int i=pos; i>=1; i--) {
 					cache[i] = cache[i-1];
 				}
-				cache[0] = x;
 			}
+			cache[0] = x;
 		}
 		return cache;
 	}

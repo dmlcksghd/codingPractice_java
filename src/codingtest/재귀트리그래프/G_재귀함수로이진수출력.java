cc19181aba862b706a0977b08a8b8aa3b6037069
HongEC <honlucete@naver.com>
2025-01-17 08:39:36 +0900

    ...

--- src/codingtest/재귀트리그래프/G_재귀함수로이진수출력.java
+++ src/codingtest/재귀트리그래프/G_재귀함수로이진수출력.java
@@ -9,8 +9,8 @@ public class G_재귀함수로이진수출력 {
 	private static void Recursive(int i) {
 		if (i==0) return;
 		else {
-			System.out.println(i%2 + " ");
 			Recursive(i/2);
+			System.out.println(i%2 + " ");
 		}
 	}
 

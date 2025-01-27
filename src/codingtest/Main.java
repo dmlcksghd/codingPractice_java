394784a9b5de7431047969a871e547b1bde4013c
HongEC <honlucete@naver.com>
2025-01-27 11:57:25 +0900

    ...

--- src/codingtest/Main.java
+++ src/codingtest/Main.java
@@ -1,12 +1,46 @@
 package codingtest;
 
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[][] people = new int[n][2];
+		
+		for (int i=0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			people[i][0] = Integer.parseInt(st.nextToken());
+			people[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		System.out.println(solution(n, people));
 
+		br.close();
+	}
+
+	private static String solution(int n, int[][] people) {
+		int[] ranks = new int[n];
+		StringBuilder sb = new StringBuilder();
+		
+		Arrays.fill(ranks, 1);
+		
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<n; j++) {
+				if (i == j) continue;
+				if (people[i][0] < people[j][0] && people[i][1] < people[j][1]) {
+					ranks[i]++;
+				}
+			}
+		}
+		
+		for (int rank : ranks) sb.append(rank).append(" ");
+		
+		return sb.toString();
 	}
 
 }
--- src/codingtest/재귀트리그래프/test.java
+++ src/codingtest/재귀트리그래프/test.java
@@ -7,29 +7,30 @@ public class test {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int n = Integer.parseInt(br.readLine());
 		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] num = new int[n];
-		for (int i=0; i<n; i++) {
-			num[i] = Integer.parseInt(br.readLine());
-		}
+		for (int i=0; i<n; i++) num[i] = Integer.parseInt(st.nextToken());
 		
-		for (int x : solution(n, num)) {
-			bw.write(x + "\n");
-		}
-		
-		bw.flush();
+		int totalTime = solution(n, num);
+		System.out.println(totalTime);
 		
 		br.close();
-		bw.close();
 	}
 
-	private static int[] solution(int n, int[] num) {
+	private static int solution(int n, int[] num) {
 		Arrays.sort(num);
 		
-		return num;
+		int totalTime = 0;
+		int accumulatedTime = 0;
+		for (int time : num) {
+            accumulatedTime += time; // 현재 사람의 대기 시간
+            totalTime += accumulatedTime; // 총 소요 시간에 더하기
+        }
+		
+		return totalTime;
 	}
 
 }
